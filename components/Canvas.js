628251d6298405b4d87402aac96ae8f42af64144
mishanya644 <ivanov0604041@gmail.com>
2022-03-06 19:42:14 +0300

    initial-commit

---
+++ components/Canvas.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../styles/Canvas.module.css';
+import Card from './Card.js';
+
+function Canvas(){
+  return <div className={styles.canvas}>
+    <Card />
+  </div>
+}
+export default Canvas;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import React, { useState } from 'react';
 import '../styles/globals.css'
+import Canvas from '../components/Canvas'
+import ModeContext from '../components/ModeContext';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [mode, setMode] = useState('managing');
+  return <ModeContext.Provider value={{ mode: mode, setMode: setMode }}>
+    <Canvas />
+  </ModeContext.Provider>
 }
 
 export default MyApp
