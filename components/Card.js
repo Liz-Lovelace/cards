509ad33f579a35ecc72c4c25dcb20d9bed844d59
mishanya644 <ivanov0604041@gmail.com>
2022-03-15 02:15:35 +0300

    freeze project

--- components/Card.js
+++ components/Card.js
@@ -4,11 +4,11 @@ import { useDrag } from '@use-gesture/react';
 import ModeContext from './ModeContext.js';
 
 function Card(props){
+  console.log('render');
   const {mode, setMode} = useContext(ModeContext);
   const [cardPos, setCardPos] = useState({x: 0, y: 0});
   const bindPos = useDrag((params)=>{
       let newX = params.offset[0];
-      console.log(params.offset[0]);
       if (newX < 0)
         newX = 0;
       setCardPos({
@@ -19,16 +19,27 @@ function Card(props){
       {bounds: {top: 0, left: 0}},
     );
   const [mouseDown, setMouseDown] = useState(false);
+  
   let classes = styles.card;
   if (mode == 'editing_card')
     classes += ' ' + styles.zoom;
+
   return (
     <div {...bindPos()}
       className={classes}
       style={{
         left: cardPos.x,
         top: cardPos.y,
-      }}> 
+      }}
+      onClick={()=>{
+        if (mode != 'editing_card')
+          setMode('editing_card');
+        else
+          setMode('managing');
+        }
+      }
+    > 
+      
 
       mode is {mode}
 
